59e4733ec1e470859e7bb49cf59b59f05cda5478
stss15 <116999988+stss15@users.noreply.github.com>
2025-11-14 06:42:18 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/IGCSEPSEUDOCODE/',
+  plugins: [react()],
+})
+
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
