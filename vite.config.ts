028aae9ec54f2a01d6d21843c029ae5a5687c540
stss15 <116999988+stss15@users.noreply.github.com>
2025-11-14 06:44:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,3 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -9,24 +5,3 @@ export default defineConfig({
   base: '/IGCSEPSEUDOCODE/',
   plugins: [react()],
 })
-
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
